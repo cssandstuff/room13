62926a0acea69063ebabda5e0ea8c4315a10a12d
Tim Clulow <tim@cssandstuff.com>
2019-09-08 21:16:40 +1200

    first

---
+++ public/bundle.js
@@ -0,0 +1,469 @@
+
+(function(l, i, v, e) { v = l.createElement(i); v.async = 1; v.src = '//' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1'; e = l.getElementsByTagName(i)[0]; e.parentNode.insertBefore(v, e)})(document, 'script');
+var app = (function () {
+    'use strict';
+
+    function noop() { }
+    function add_location(element, file, line, column, char) {
+        element.__svelte_meta = {
+            loc: { file, line, column, char }
+        };
+    }
+    function run(fn) {
+        return fn();
+    }
+    function blank_object() {
+        return Object.create(null);
+    }
+    function run_all(fns) {
+        fns.forEach(run);
+    }
+    function is_function(thing) {
+        return typeof thing === 'function';
+    }
+    function safe_not_equal(a, b) {
+        return a != a ? b == b : a !== b || ((a && typeof a === 'object') || typeof a === 'function');
+    }
+
+    function append(target, node) {
+        target.appendChild(node);
+    }
+    function insert(target, node, anchor) {
+        target.insertBefore(node, anchor || null);
+    }
+    function detach(node) {
+        node.parentNode.removeChild(node);
+    }
+    function destroy_each(iterations, detaching) {
+        for (let i = 0; i < iterations.length; i += 1) {
+            if (iterations[i])
+                iterations[i].d(detaching);
+        }
+    }
+    function element(name) {
+        return document.createElement(name);
+    }
+    function text(data) {
+        return document.createTextNode(data);
+    }
+    function space() {
+        return text(' ');
+    }
+    function attr(node, attribute, value) {
+        if (value == null)
+            node.removeAttribute(attribute);
+        else
+            node.setAttribute(attribute, value);
+    }
+    function children(element) {
+        return Array.from(element.childNodes);
+    }
+    function set_data(text, data) {
+        data = '' + data;
+        if (text.data !== data)
+            text.data = data;
+    }
+    function set_style(node, key, value, important) {
+        node.style.setProperty(key, value, important ? 'important' : '');
+    }
+
+    let current_component;
+    function set_current_component(component) {
+        current_component = component;
+    }
+    function get_current_component() {
+        if (!current_component)
+            throw new Error(`Function called outside component initialization`);
+        return current_component;
+    }
+    function onMount(fn) {
+        get_current_component().$$.on_mount.push(fn);
+    }
+
+    const dirty_components = [];
+    const binding_callbacks = [];
+    const render_callbacks = [];
+    const flush_callbacks = [];
+    const resolved_promise = Promise.resolve();
+    let update_scheduled = false;
+    function schedule_update() {
+        if (!update_scheduled) {
+            update_scheduled = true;
+            resolved_promise.then(flush);
+        }
+    }
+    function add_render_callback(fn) {
+        render_callbacks.push(fn);
+    }
+    function flush() {
+        const seen_callbacks = new Set();
+        do {
+            // first, call beforeUpdate functions
+            // and update components
+            while (dirty_components.length) {
+                const component = dirty_components.shift();
+                set_current_component(component);
+                update(component.$$);
+            }
+            while (binding_callbacks.length)
+                binding_callbacks.pop()();
+            // then, once components are updated, call
+            // afterUpdate functions. This may cause
+            // subsequent updates...
+            for (let i = 0; i < render_callbacks.length; i += 1) {
+                const callback = render_callbacks[i];
+                if (!seen_callbacks.has(callback)) {
+                    callback();
+                    // ...so guard against infinite loops
+                    seen_callbacks.add(callback);
+                }
+            }
+            render_callbacks.length = 0;
+        } while (dirty_components.length);
+        while (flush_callbacks.length) {
+            flush_callbacks.pop()();
+        }
+        update_scheduled = false;
+    }
+    function update($$) {
+        if ($$.fragment) {
+            $$.update($$.dirty);
+            run_all($$.before_update);
+            $$.fragment.p($$.dirty, $$.ctx);
+            $$.dirty = null;
+            $$.after_update.forEach(add_render_callback);
+        }
+    }
+    const outroing = new Set();
+    function transition_in(block, local) {
+        if (block && block.i) {
+            outroing.delete(block);
+            block.i(local);
+        }
+    }
+    function mount_component(component, target, anchor) {
+        const { fragment, on_mount, on_destroy, after_update } = component.$$;
+        fragment.m(target, anchor);
+        // onMount happens before the initial afterUpdate
+        add_render_callback(() => {
+            const new_on_destroy = on_mount.map(run).filter(is_function);
+            if (on_destroy) {
+                on_destroy.push(...new_on_destroy);
+            }
+            else {
+                // Edge case - component was destroyed immediately,
+                // most likely as a result of a binding initialising
+                run_all(new_on_destroy);
+            }
+            component.$$.on_mount = [];
+        });
+        after_update.forEach(add_render_callback);
+    }
+    function destroy_component(component, detaching) {
+        if (component.$$.fragment) {
+            run_all(component.$$.on_destroy);
+            component.$$.fragment.d(detaching);
+            // TODO null out other refs, including component.$$ (but need to
+            // preserve final state?)
+            component.$$.on_destroy = component.$$.fragment = null;
+            component.$$.ctx = {};
+        }
+    }
+    function make_dirty(component, key) {
+        if (!component.$$.dirty) {
+            dirty_components.push(component);
+            schedule_update();
+            component.$$.dirty = blank_object();
+        }
+        component.$$.dirty[key] = true;
+    }
+    function init(component, options, instance, create_fragment, not_equal, prop_names) {
+        const parent_component = current_component;
+        set_current_component(component);
+        const props = options.props || {};
+        const $$ = component.$$ = {
+            fragment: null,
+            ctx: null,
+            // state
+            props: prop_names,
+            update: noop,
+            not_equal,
+            bound: blank_object(),
+            // lifecycle
+            on_mount: [],
+            on_destroy: [],
+            before_update: [],
+            after_update: [],
+            context: new Map(parent_component ? parent_component.$$.context : []),
+            // everything else
+            callbacks: blank_object(),
+            dirty: null
+        };
+        let ready = false;
+        $$.ctx = instance
+            ? instance(component, props, (key, value) => {
+                if ($$.ctx && not_equal($$.ctx[key], $$.ctx[key] = value)) {
+                    if ($$.bound[key])
+                        $$.bound[key](value);
+                    if (ready)
+                        make_dirty(component, key);
+                }
+            })
+            : props;
+        $$.update();
+        ready = true;
+        run_all($$.before_update);
+        $$.fragment = create_fragment($$.ctx);
+        if (options.target) {
+            if (options.hydrate) {
+                // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+                $$.fragment.l(children(options.target));
+            }
+            else {
+                // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+                $$.fragment.c();
+            }
+            if (options.intro)
+                transition_in(component.$$.fragment);
+            mount_component(component, options.target, options.anchor);
+            flush();
+        }
+        set_current_component(parent_component);
+    }
+    class SvelteComponent {
+        $destroy() {
+            destroy_component(this, 1);
+            this.$destroy = noop;
+        }
+        $on(type, callback) {
+            const callbacks = (this.$$.callbacks[type] || (this.$$.callbacks[type] = []));
+            callbacks.push(callback);
+            return () => {
+                const index = callbacks.indexOf(callback);
+                if (index !== -1)
+                    callbacks.splice(index, 1);
+            };
+        }
+        $set() {
+            // overridden by instance, if it has props
+        }
+    }
+    class SvelteComponentDev extends SvelteComponent {
+        constructor(options) {
+            if (!options || (!options.target && !options.$$inline)) {
+                throw new Error(`'target' is a required option`);
+            }
+            super();
+        }
+        $destroy() {
+            super.$destroy();
+            this.$destroy = () => {
+                console.warn(`Component was already destroyed`); // eslint-disable-line no-console
+            };
+        }
+    }
+
+    /* src/App.svelte generated by Svelte v3.9.2 */
+
+    const file = "src/App.svelte";
+
+    function get_each_context(ctx, list, i) {
+    	const child_ctx = Object.create(ctx);
+    	child_ctx.c = list[i];
+    	return child_ctx;
+    }
+
+    // (103:0) {#each confetti as c}
+    function create_each_block(ctx) {
+    	var span, t_value = ctx.c.character + "", t;
+
+    	return {
+    		c: function create() {
+    			span = element("span");
+    			t = text(t_value);
+    			set_style(span, "transform", "translateX(" + ctx.c.x + "vw) translateY(" + ctx.c.y + "vh) translateZ(" + ctx.c.y + "vh) rotateZ(" + ctx.c.y/300 * 90 + "deg) rotateX(" + ctx.c.y/100 * 90 + "deg) scale(" + ctx.c.s + ")");
+    			set_style(span, "opacity", ctx.c.y / 20);
+    			attr(span, "class", "svelte-16wjkl6");
+    			add_location(span, file, 103, 1, 1863);
+    		},
+
+    		m: function mount(target, anchor) {
+    			insert(target, span, anchor);
+    			append(span, t);
+    		},
+
+    		p: function update(changed, ctx) {
+    			if ((changed.confetti) && t_value !== (t_value = ctx.c.character + "")) {
+    				set_data(t, t_value);
+    			}
+
+    			if (changed.confetti) {
+    				set_style(span, "transform", "translateX(" + ctx.c.x + "vw) translateY(" + ctx.c.y + "vh) translateZ(" + ctx.c.y + "vh) rotateZ(" + ctx.c.y/300 * 90 + "deg) rotateX(" + ctx.c.y/100 * 90 + "deg) scale(" + ctx.c.s + ")");
+    				set_style(span, "opacity", ctx.c.y / 20);
+    			}
+    		},
+
+    		d: function destroy(detaching) {
+    			if (detaching) {
+    				detach(span);
+    			}
+    		}
+    	};
+    }
+
+    function create_fragment(ctx) {
+    	var div, h1, t_1;
+
+    	var each_value = ctx.confetti;
+
+    	var each_blocks = [];
+
+    	for (var i = 0; i < each_value.length; i += 1) {
+    		each_blocks[i] = create_each_block(get_each_context(ctx, each_value, i));
+    	}
+
+    	return {
+    		c: function create() {
+    			div = element("div");
+    			h1 = element("h1");
+    			h1.textContent = "Thank You Room 13!";
+    			t_1 = space();
+
+    			for (var i = 0; i < each_blocks.length; i += 1) {
+    				each_blocks[i].c();
+    			}
+    			attr(h1, "class", "svelte-16wjkl6");
+    			add_location(h1, file, 101, 0, 1812);
+    			attr(div, "class", "threeD svelte-16wjkl6");
+    			add_location(div, file, 100, 0, 1791);
+    		},
+
+    		l: function claim(nodes) {
+    			throw new Error("options.hydrate only works if the component was compiled with the `hydratable: true` option");
+    		},
+
+    		m: function mount(target, anchor) {
+    			insert(target, div, anchor);
+    			append(div, h1);
+    			append(div, t_1);
+
+    			for (var i = 0; i < each_blocks.length; i += 1) {
+    				each_blocks[i].m(div, null);
+    			}
+    		},
+
+    		p: function update(changed, ctx) {
+    			if (changed.confetti) {
+    				each_value = ctx.confetti;
+
+    				for (var i = 0; i < each_value.length; i += 1) {
+    					const child_ctx = get_each_context(ctx, each_value, i);
+
+    					if (each_blocks[i]) {
+    						each_blocks[i].p(changed, child_ctx);
+    					} else {
+    						each_blocks[i] = create_each_block(child_ctx);
+    						each_blocks[i].c();
+    						each_blocks[i].m(div, null);
+    					}
+    				}
+
+    				for (; i < each_blocks.length; i += 1) {
+    					each_blocks[i].d(1);
+    				}
+    				each_blocks.length = each_value.length;
+    			}
+    		},
+
+    		i: noop,
+    		o: noop,
+
+    		d: function destroy(detaching) {
+    			if (detaching) {
+    				detach(div);
+    			}
+
+    			destroy_each(each_blocks, detaching);
+    		}
+    	};
+    }
+
+    function instance($$self, $$props, $$invalidate) {
+    	let characters = [
+    		'Maja', 'Payton', 'Miss Quach',
+    		'Reef', 'Rylee', 'Noa',
+    		'Sophie', 'Aayah', 'Olive',
+    		'Abbie', 'Lucy', 'Cruz',
+    		'Benson', 'Zion', 'Ariyah',
+    		'Milly', 'Shekinah', 'Onyx',
+    		'Nathan', 'Johnny', 'John',
+    		'Frank', 'Ethan', 'Heath',
+    		'Kobey', 'Reilloch', 'Zachary',
+    		'Jaxon',
+    		];
+
+    	let emoji = [
+    		'🥳', '🎉', '✨',
+    		'🐠', '🤪', '❤️',
+    		'🥺', '😻', '😜',
+    		'😎', '😘', '🙄',
+    		'🤩', '👍', '🤟',
+    		'👌', '🙌', '👏',
+    		'😋', '🐶', '🐹',
+    		'🦁', '🐭', '🤓',
+    		'🌸', '🏅', '🏆',
+    		'⭐️',
+    		];
+
+
+    	let confetti = new Array(40).fill()
+    		.map((_, i) => {
+    			return {
+    				character: emoji[i % emoji.length] + characters[i % characters.length],
+    				x: (Math.random() * 50) + 10,
+    				y: -20 - Math.random() * 400,
+    				r: 0.2 + Math.random() * 0.5,
+    				s: (Math.random() * 0.55) + 0.3,
+    			};
+    		})
+    		.sort((a, b) => a.r - b.r);
+
+    	onMount(() => {
+    		let frame;
+
+    		function loop() {
+    			frame = requestAnimationFrame(loop);
+
+    			$$invalidate('confetti', confetti = confetti.map(emoji => {
+    				emoji.y += 0.7 * emoji.r;
+    				if (emoji.y > 90) emoji.y = -90;
+    				return emoji;
+    			}));
+    		}
+
+    		loop();
+
+    		return () => cancelAnimationFrame(frame);
+    	});
+
+    	return { confetti };
+    }
+
+    class App extends SvelteComponentDev {
+    	constructor(options) {
+    		super(options);
+    		init(this, options, instance, create_fragment, safe_not_equal, []);
+    	}
+    }
+
+    const app = new App({
+    	target: document.body,
+    	props: {
+    		name: 'world'
+    	}
+    });
+
+    return app;
+
+}());
+//# sourceMappingURL=bundle.js.map
---
+++ src/App.svelte
@@ -0,0 +1,106 @@
+<script>
+	import { onMount } from 'svelte';
+
+	let characters = [
+		'Maja', 'Payton', 'Miss Quach',
+		'Reef', 'Rylee', 'Noa',
+		'Sophie', 'Aayah', 'Olive',
+		'Abbie', 'Lucy', 'Cruz',
+		'Benson', 'Zion', 'Ariyah',
+		'Milly', 'Shekinah', 'Onyx',
+		'Nathan', 'Johnny', 'John',
+		'Frank', 'Ethan', 'Heath',
+		'Kobey', 'Reilloch', 'Zachary',
+		'Jaxon',
+		];
+
+	let emoji = [
+		'🥳', '🎉', '✨',
+		'🐠', '🤪', '❤️',
+		'🥺', '😻', '😜',
+		'😎', '😘', '🙄',
+		'🤩', '👍', '🤟',
+		'👌', '🙌', '👏',
+		'😋', '🐶', '🐹',
+		'🦁', '🐭', '🤓',
+		'🌸', '🏅', '🏆',
+		'⭐️',
+		];
+
+
+	let confetti = new Array(40).fill()
+		.map((_, i) => {
+			return {
+				character: emoji[i % emoji.length] + characters[i % characters.length],
+				x: (Math.random() * 50) + 10,
+				y: -20 - Math.random() * 400,
+				r: 0.2 + Math.random() * 0.5,
+				s: (Math.random() * 0.55) + 0.3,
+			};
+		})
+		.sort((a, b) => a.r - b.r);
+
+	onMount(() => {
+		let frame;
+
+		function loop() {
+			frame = requestAnimationFrame(loop);
+
+			confetti = confetti.map(emoji => {
+				emoji.y += 0.7 * emoji.r;
+				if (emoji.y > 90) emoji.y = -90;
+				return emoji;
+			});
+		}
+
+		loop();
+
+		return () => cancelAnimationFrame(frame);
+	});
+</script>
+
+<style>
+	:global(body) {
+		overflow: hidden;
+		background: rgb(146, 91, 128);
+		font-family: 'Caveat', cursive;
+	}
+
+	span {
+		position: absolute;
+		font-size: 5vw;
+		padding: 0.4em;
+		background: #111;
+		color: #fff;
+		border-radius: 10px;
+		font-weight: 300;
+		top: -90px;
+		display: block;
+	}
+	.threeD{
+		perspective: 900px
+	}
+	h1{
+		display: flex;
+		align-items: center;
+		background: #fff;
+		color: #000;
+		font-weight: 500;
+		padding: 1em;
+		z-index: 99;
+		left: 50%;
+		margin-left: -200px;
+		text-align: center;
+		top: 0;
+		margin-top: -15px;
+		position: absolute;
+		border-radius: 5px;
+	}
+</style>
+
+<div class="threeD">
+<h1>Thank You Room 13!</h1>
+{#each confetti as c}
+	<span style="transform: translateX({c.x}vw) translateY({c.y}vh) translateZ({c.y}vh) rotateZ({c.y/300 * 90}deg) rotateX({c.y/100 * 90}deg) scale({c.s}); opacity:{c.y / 20}">{c.character}</span>
+{/each}
+</div>
